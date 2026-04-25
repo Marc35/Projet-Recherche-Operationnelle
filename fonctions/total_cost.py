1afd145c3b5b702233906b03d13bdcce986066bb
Thomas Duriaud <46865549+Marc35@users.noreply.github.com>
2026-04-26 00:18:20 +0200

    Update total_cost.py

--- fonctions/total_cost.py
+++ fonctions/total_cost.py
@@ -1,4 +1,7 @@
 def total_cost(cost_matrix, transport_proposition):
+    """
+    Calcul le cout total d'une proposition de transport, avec la matrice de la proposition et la matrice des couts
+    """
     return sum(
         cost_matrix[i][j] * transport_proposition[i][j]
         for i in range(len(cost_matrix))
